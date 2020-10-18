bf1f0db357f99f42b6ed3f755dd5eeeb61126bc2
Jairo Chapela-Martinez <jairochapela@gmail.com>
2020-10-18 19:50:15 +0200

    Form submission processor (work in progress)

---
+++ static/form.php
@@ -0,0 +1,27 @@
+<?php
+require_once './vendor/autoload.php';
+
+use PHPMailer\PHPMailer\PHPMailer;
+
+
+$mustache = new Mustache_Engine(array(
+    'template_class_prefix' => '__MyTemplates_',
+    // 'cache' => dirname(__FILE__).'/tmp/cache/mustache',
+    // 'cache_file_mode' => 0666, // Please, configure your umask instead of doing this :)
+    'cache_lambda_templates' => true,
+    'loader' => new Mustache_Loader_FilesystemLoader(dirname(__FILE__).'/mail-templates'),
+    'partials_loader' => new Mustache_Loader_FilesystemLoader(dirname(__FILE__).'/views/partials'),
+    'helpers' => array('i18n' => function($text) {
+        // do something translatey here...
+    }),
+    'escape' => function($value) {
+        return htmlspecialchars($value, ENT_COMPAT, 'UTF-8');
+    },
+    'charset' => 'ISO-8859-1',
+    'logger' => new Mustache_Logger_StreamLogger('php://stderr'),
+    'strict_callables' => true,
+    'pragmas' => [Mustache_Engine::PRAGMA_FILTERS],
+));
+
+$tpl = $mustache->loadTemplate('contact'); // loads __DIR__.'/mail-templates/foo.mustache';
+echo $tpl->render(array('planet' => 'Earth'));
