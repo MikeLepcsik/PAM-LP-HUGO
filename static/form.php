528197f023bd91e5deb87f227a5b5d4bbeb0c3bf
mike <Mike.Lepcsik@cib.de>
2020-11-14 19:17:04 +0100

    update form

--- static/form.php
+++ static/form.php
@@ -32,15 +32,35 @@ $this_message_is_spam = false; //TODO: perform some validation
 
 $subject = "Contact Form"; //TODO: get from some hidden field in form data
 $code = isset($_GET['code'])? $_GET['code'] : 'contact';
-
-// load template and render html and text versions
-$tpl_html = $mustache->loadTemplate("$code.html"); // loads __DIR__.'/mail-templates/foo.mustache';
-$html = $tpl_html->render($_POST);
-$tpl_txt = $mustache->loadTemplate("$code.txt");
-$txt = $tpl_txt->render($_POST);
+$reply = isset($_GET['reply']) && isset($_POST['eMail'])? $_GET['reply'] : false;
 
 // create a new mailer object
 $mail = new PHPMailer();
+
+// add attachments
+$filenames = [];
+
+foreach ($_FILES as $file) {
+    if (is_array($file['name'])) {
+        for ($i=0; $i < count($file['name']); $i++) { 
+            $mail->addAttachment($file['tmp_name'][$i], $file['name'][$i]);
+            $filenames[] = $file['name'][$i];
+        }
+    } else {
+        $mail->addAttachment($file['tmp_name'], $file['name']);
+        $filenames = $file['name'];
+    }
+}
+
+// load template and render html and text versions
+$data = array_merge($_POST, ['files' => $filenames]);
+
+$tpl_html = $mustache->loadTemplate("$code.html"); // loads __DIR__.'/mail-templates/foo.mustache';
+$html = $tpl_html->render($data);
+$tpl_txt = $mustache->loadTemplate("$code.txt");
+$txt = $tpl_txt->render($data);
+
+
 // configure SMTP
 $mail->isSMTP();
 $mail->Host = $_ENV['SMTP_HOST'];
@@ -60,16 +80,6 @@ $mail->isHTML(TRUE);
 $mail->Body = $html;
 $mail->AltBody = $txt;
 
-// add attachments
-foreach ($_FILES as $file) {
-    if (is_array($file['name'])) {
-        for ($i=0; $i < count($file['name']); $i++) { 
-            $mail->addAttachment($file['tmp_name'][$i], $file['name'][$i]);
-        }
-    } else {
-        $mail->addAttachment($file['tmp_name'], $file['name']);
-    }
-}
 
 // send the message
 if(!$mail->send()){
@@ -79,61 +89,42 @@ if(!$mail->send()){
     echo 'Message has been sent';
 }
 
-?>
 
-<!DOCTYPE html>
-<html xmlns='http://www.w3.org/1999/xhtml' xmlns:v='urn:schemas-microsoft-com:vml' xmlns:o='urn:schemas-microsoft-com:office:office'>
 
-<head>
-    <title>Abonnementanfrage erhalten</title>
-    <meta charset='UTF-8'>
-        <meta http-equiv='X-UA-Compatible' content='IE=edge'>
-        <meta name='viewport' content='width=device-width, initial-scale=1'>
-    <style>
-table {
-  font-family: Segoe UI, Arial, Helvetica, sans-serif;
-  border-collapse: collapse;
-  width: 100%;
+// Reply email
+if ($reply) {
+    $mail = new PHPMailer();
+
+    $tpl_html = $mustache->loadTemplate("reply.html"); // loads __DIR__.'/mail-templates/foo.mustache';
+    $html = $tpl_html->render($data);
+    $tpl_txt = $mustache->loadTemplate("reply.txt");
+    $txt = $tpl_txt->render($data);
+
+
+    // configure SMTP
+    $mail->isSMTP();
+    $mail->Host = $_ENV['SMTP_HOST'];
+    $mail->SMTPAuth = true;
+    $mail->Username = $_ENV['SMTP_USERNAME'];
+    $mail->Password = $_ENV['SMTP_PASSWORD'];
+    $mail->SMTPSecure = $_ENV['SMTP_SECURITY'];
+    $mail->Port = $_ENV['SMTP_PORT'];
+
+    $mail->setFrom($_ENV['FROM_EMAIL_ADDRESS'], $_ENV['APP_NAME']);
+    $mail->addAddress($_POST['eMail']);
+    $mail->addCustomHeader('X-Spam-Status', $this_message_is_spam? 'Yes' : 'No');
+    $mail->Subject = ($this_message_is_spam? '*** SPAM *** ' : '') . '[' . $_ENV['APP_NAME'] . '] ' . $subject;
+
+    // Set HTML 
+    $mail->isHTML(TRUE);
+    $mail->Body = $html;
+    $mail->AltBody = $txt;
+
+    // send the message
+    if(!$mail->send()){
+        echo 'Reply message could not be sent.';
+        echo 'Mailer Error: ' . $mail->ErrorInfo;
+    } else {
+        echo 'Reply message has been sent';
+    }
 }
-
-td, th {
-  border: 1px solid #bec8d2;
-  text-align: left;
-  padding: 8px;
-}
-
-tr:nth-child(even) {
-  background-color: #dee3e8;
-}
-</style>
-</head>
-
-<body>
-<div style='padding: 20px; text-align: center; background: #ffffff; border-radius: 5px; margin: 40px auto; margin-bottom: 0px; font-family: Segoe UI, Arial, Helvetica, sans-serif; font-size: 15px; color: #666;'>
-
-    <img width="100"
-        vspace="0"
-        hspace="0"
-        border="0"
-        alt="CIB"
-        style="text-align: center;"
-        class="rnb-logo-img"
-        src="img/CIB_Logo.svg"></div>
-
-<div style='color: #999; padding: 20px 20px; text-align: center;'>
-<div style='color: #444444; font-weight: normal; text-align: center;'>
-    <div style='padding: 30px 0; font-size: 24px; font-family: Segoe UI, Arial, Helvetica, sans-serif; color: #555; text-align: center; line-height: 30px;'>
-    Vielen Dank! </div>
-</div>
-<div style='color: #444444; font-weight: normal; text-align: center;'>
-    <div style='padding: 30px 0; font-size: 18px; font-family: Segoe UI, Arial, Helvetica, sans-serif; color: #555; text-align: center; line-height: 30px;'>
-    Ihre Nachricht wurde erfolgreich versendet.</div>
-</div>
-</div>
-</br>
-</br>
-</body>
-
-</html>";
-
-<?php
